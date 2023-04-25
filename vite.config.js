a07f26541d4a6121a43cff449f509c8a4d4b827b
porfinogeneta <szymnmazurek@gmail.com>
2023-04-25 14:28:30 -0400

    fixing vite config

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   // Get the correct base URL depending on the build mode
-  const base = mode === 'production' ? '/yourprojectname/' : '/';
 
   return {
     base: '/<REPO>/',

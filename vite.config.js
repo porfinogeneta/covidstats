d8a19df42b04bcd50bf31b1b6b9d79fb088b4b33
porfinogeneta <szymnmazurek@gmail.com>
2023-04-25 12:50:08 -0400

    fixing vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'docs',
-  }
-})
+export default defineConfig(({ mode }) => {
+  // Get the correct base URL depending on the build mode
+  const base = mode === 'production' ? '/yourprojectname/' : '/';
+
+  return {
+    base,
+    plugins: [react(), reactRefresh()],
+    build: {
+      outDir: 'docs',
+    },
+  };
+});
